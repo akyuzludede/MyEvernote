8fd2da7076cbd4a8ac65cdb89e7f3a1702841c5f
Cagatay <Cagatay@TANIMLANAMIYOR>
2024-02-15 22:26:49 +0300

    Add project files.

---
+++ MyEvernote.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEvernote.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            /* Bu kısım dbcontext ilk halindeyken ki test ettiğimiz komutlar.
+            BusinessLayer.DBCreatedOn dBCreatedOn = new BusinessLayer.DBCreatedOn();
+            dBCreatedOn.Insert_Test();
+            dBCreatedOn.Update_Test();
+            dBCreatedOn.Delete_Test();
+            dBCreatedOn.Insert_Comment();
+            */
+
+            return View();
+        }
+    }
+}
